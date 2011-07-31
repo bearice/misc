fce30f91244ae690526dfc61fe61690b058129b9
Bearice <bearice@gmail.com>
2011-07-31 14:36:29 +0800

    fix apply.sh

--- apply.sh
+++ apply.sh
@@ -2,7 +2,7 @@
 mv ~/.vimrc ~/.vimrc.bak
 mv ~/.zshrc ~/.zshrc.bak
 #mv ~/.oh-my-zsh ~/.oh-my-zsh.bak
-ln ~/.vimrc vimrc
-ln ~/.zshrc zshrc
+ln vimrc ~/.vimrc 
+ln zshrc ~/.zshrc 
 #ln ~/.oh-my-zsh oh-my-zsh
 
